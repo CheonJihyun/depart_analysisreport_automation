95b76bd87a2b6c5d897f3862cf50fec7c5c4ce8b
Cheonji <jhyun1134@gmail.com>
2026-04-02 18:34:28 +0900

    260402_final

--- main.py
+++ main.py
@@ -504,13 +504,13 @@ def run():
     start_time = time.time()
 
     config = {
-        "target_id": 5,
-        "fb_ad_account_id":"act_1073418823880031",
-        "start":"2025-04-14",
-        "end": "2026-01-25",
-        "main_age": ["25-34","35-44"],
+        "target_id": 12,
+        "fb_ad_account_id":"act_1358317995522216",
+        "start":"2025-10-27",
+        "end": "2026-03-29",
+        "main_age": ["35-44","45-54"],
         "main_gender": "",
-        "avoid_age": "",
+        "avoid_age": "65+",
         "avoid_gender": "",
     }
     target_id, fb_ad_account_id = config["target_id"], config["fb_ad_account_id"]
@@ -529,7 +529,7 @@ def run():
                     avoid_age=avoid_age, avoid_gender=avoid_gender)
     
     report_path = "json_reports/integrated_report.json"
-    theme_color = "#3E2753"
+    theme_color = "#0076BD"
 
     report_json = _load_report(report_path)
     _apply_display_predicate_suffix(report_json)
